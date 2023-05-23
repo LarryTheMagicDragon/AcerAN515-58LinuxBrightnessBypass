b622cca99bd2bf5fb5b92705d76c61138e11f318
LarryTheMagicDragon <mendelbaker@gmail.com>
2023-05-22 20:27:23 -0400

    this may just work

--- install_brightness_bypass.sh
+++ install_brightness_bypass.sh
@@ -1,37 +1,18 @@
 !#bin/bash
-#looks like we're just gonna have to run it as root
 apt install inotify-tools
 
 touch /etc/systemd/system/brightness-hotfix.service
 
-sudo chmod 744 /etc/systemd/system/brightness-hotfix.service
+chmod 744 /etc/systemd/system/brightness-hotfix.service
 
-sudo cat "[Unit]
-Description=File Monitoring Service
-After=network.target
+echo '[Unit]\nDescription=File Monitoring Service\nAfter=network.target\n\n[Service]\nExecStart=/bin/bash /usr/local/bin/backlight-hotfix.sh\nUser=root\n\n[Install]\nWantedBy=default.target' > /etc/systemd/system/brightness-hotfix.service
 
-[Service]
-ExecStart=/bin/bash /usr/local/bin/backlight-hotfix.sh
-User=root
+touch /usr/local/bin/backlight-hotfix.sh
 
-[Install]
-WantedBy=default.target" > /etc/systemd/system/brightness-hotfix.service
+chmod 744 /usr/local/bin/backlight-hotfix.sh
 
-sudo chmod 644 /etc/systemd/system/brightness-hotfix.service
+cat > '#!/bin/bash\nwhile inotifywait -e modify /sys/class/backlight/nvidia_wmi_ec_backlight/brightness; do\n    VALUE=$(cat /sys/class/backlight/nvidia_wmi_ec_backlight/brightness)\n    NEWVALUE=$(($VALUE * 75))\n    echo $NEWVALUE > /sys/class/backlight/intel_backlight/brightness\ndone'
 
-sudo touch /usr/local/bin/backlight-hotfix.sh
-
-sudo chmod 744 /usr/local/bin/backlight-hotfix.sh
-
-sudo cat > '#!/bin/bash
-while inotifywait -e modify /sys/class/backlight/nvidia_wmi_ec_backlight/brightness; do
-    VALUE=$(cat /sys/class/backlight/nvidia_wmi_ec_backlight/brightness)
-    NEWVALUE=$(($VALUE * 75))
-    echo $NEWVALUE > /sys/class/backlight/intel_backlight/brightness
-done'
-
-sudo chmod 644 /usr/local/bin/backlight-hotfix.sh
-
-sudo systemctl daemon-reload
-sudo systemctl enable brightness-hotfix.service
-sudo systemctl start file_monitor.service
+systemctl daemon-reload
+systemctl enable brightness-hotfix.service
+systemctl start brightness-hotfix.service
