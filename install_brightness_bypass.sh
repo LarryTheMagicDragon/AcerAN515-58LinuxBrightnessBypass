a0879f6dc2ed0a21dfadb7cf43b288b642d22ab4
LarryTheMagicDragon <92491710+LarryTheMagicDragon@users.noreply.github.com>
2023-05-22 21:20:07 -0400

    Update install_brightness_bypass.sh

--- install_brightness_bypass.sh
+++ install_brightness_bypass.sh
@@ -1,18 +1,1 @@
 !#bin/bash
-apt install inotify-tools
-
-touch /etc/systemd/system/brightness-hotfix.service
-
-chmod 744 /etc/systemd/system/brightness-hotfix.service
-
-echo '[Unit]\nDescription=File Monitoring Service\nAfter=network.target\n\n[Service]\nExecStart=/bin/bash /usr/local/bin/backlight-hotfix.sh\nUser=root\n\n[Install]\nWantedBy=default.target' > /etc/systemd/system/brightness-hotfix.service
-
-touch /usr/local/bin/backlight-hotfix.sh
-
-chmod 744 /usr/local/bin/backlight-hotfix.sh
-
-cat > '#!/bin/bash\nwhile inotifywait -e modify /sys/class/backlight/nvidia_wmi_ec_backlight/brightness; do\n    VALUE=$(cat /sys/class/backlight/nvidia_wmi_ec_backlight/brightness)\n    NEWVALUE=$(($VALUE * 75))\n    echo $NEWVALUE > /sys/class/backlight/intel_backlight/brightness\ndone'
-
-systemctl daemon-reload
-systemctl enable brightness-hotfix.service
-systemctl start brightness-hotfix.service
